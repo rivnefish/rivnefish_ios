471b7a1d7a09f3f9b87100994e79dce74fd8484f
akyryl <akyryl@macmini57.ddns.softservecom.com>
2015-09-06 18:00:17 +0300

    Added my clustering way (disabled for new), read all marker data from server, added map annotations, added spot controller that is shown on annotation tap

---
+++ swift/RivneFish/SpotViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SpotViewController.swift
+//  RivneFish
+//
+//  Created by akyryl on 06/09/2015.
+//  Copyright (c) 2015 rivnefish. All rights reserved.
+//
+
+import UIKit
+
+class SpotViewController: UIViewController {
+
+}
