e83234aada390332188c38397a68079c782fb13e
Anatoliy Kyryliuk <akyryl@rvmacmini57.ddns.softservecom.com>
2015-11-09 11:54:49 +0200

    Set status bar text color to white

--- swift/RivneFish/AppDelegate.swift
+++ swift/RivneFish/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
+        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: false)
         GMSServices.provideAPIKey(googleMapsApiKey)
         return true
     }
