bd1a227d9c98c2c4146c052cd1095a936e7e7a53
Anatolii Kyryliuk <anatoliykyryliuk@gmail.com>
2016-08-16 19:45:49 +0200

    Added phone number formatting

--- swift/RivneFish/model/Marker.swift
+++ swift/RivneFish/model/Marker.swift
@@ -72,7 +72,25 @@ class MarkerModel: NSObject, NSCoding {
     var country: String?
     var photoUrls: Array<String>?
     var url: String?
-    
+
+    var contactReadable: String? {
+        if let tel = contact {
+            var s = tel
+            if s.characters.first != "+" {
+                s.insert("+", atIndex: s.characters.startIndex)
+            }
+            if s.characters.count >= 13 {
+                return String(format: "%@ (%@) %@ %@ %@", s.substringToIndex(s.startIndex.advancedBy(4)),
+                              s.substringWithRange(s.startIndex.advancedBy(4) ... s.startIndex.advancedBy(5)),
+                              s.substringWithRange(s.startIndex.advancedBy(6) ... s.startIndex.advancedBy(8)),
+                              s.substringWithRange(s.startIndex.advancedBy(9) ... s.startIndex.advancedBy(10)),
+                              s.substringWithRange(s.startIndex.advancedBy(11) ... s.startIndex.advancedBy(12))
+                )
+            }
+        }
+        return contact
+    }
+
     var areaStr: String? {
         if let val = area where val != 0.0 {
             return NSLocalizedString("\(val / 100) Га", comment: "area")
