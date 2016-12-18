22e18b1d06aa3fabc35c2b00eadde95640e7d604
Anatolii Kyryliuk <anatoliykyryliuk@gmail.com>
2016-12-18 17:49:06 +0100

    Updated colors for markers

--- swift/RivneFish/libs/GClustering/Clustering/View/GDefaultClusterRenderer.m
+++ swift/RivneFish/libs/GClustering/Clustering/View/GDefaultClusterRenderer.m
@@ -46,19 +46,31 @@
 - (UIImage*)generateClusterIconWithCount:(NSUInteger)count {
     
     int diameter = 30;
-    float inset = 2;
-    
-    CGRect rect = CGRectMake(0, 0, diameter, diameter);
+    float inset = 1;
+
+    CGRect rect = CGRectMake(0, 0, diameter + 10, diameter + 10);
     UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
 
     CGContextRef ctx = UIGraphicsGetCurrentContext();
 
+    // draw shadow
+    CGContextSaveGState(ctx);
+    CGContextSetShadow(ctx, CGSizeMake(2, 2), 2);
+    [[UIColor whiteColor] setFill];
+    CGRect shadowRect = CGRectMake(0, 0, diameter, diameter);
+    CGContextFillEllipseInRect(ctx, shadowRect);
+    CGContextRestoreGState(ctx);
+
     // set stroking color and draw circle
-    [[UIColor colorWithRed:1 green:1 blue:1 alpha:0.8] setStroke];
-    
-    if (count > 100) [[UIColor orangeColor] setFill];
-    else if (count > 10) [[UIColor yellowColor] setFill];
-    else [[UIColor colorWithRed:0.0/255.0 green:100.0/255.0 blue:255.0/255.0 alpha:1] setFill];
+    [[UIColor colorWithRed:1 green:1 blue:1 alpha:1.0] setStroke];
+
+    UIColor *yellow = [UIColor colorWithRed:1.0 green:180.0 / 255.0 blue:0 alpha:1.0];
+    UIColor *orange = [UIColor colorWithRed:253.0 / 255.0 green:128.0 / 255.0 blue:35.0 / 255.0 alpha:1.0];
+    UIColor *blue = [UIColor colorWithRed:92.0 / 255.0 green:115.0 / 255.0 blue: 201.0 / 255.0 alpha:1.0];
+
+    if (count > 100) [yellow setFill];
+    else if (count > 10) [orange setFill];
+    else [blue setFill];
 
     CGContextSetLineWidth(ctx, inset);
 
@@ -70,11 +82,10 @@
     CGContextFillEllipseInRect(ctx, circleRect);
     CGContextStrokeEllipseInRect(ctx, circleRect);
 
-    CTFontRef myFont = CTFontCreateWithName( (CFStringRef)@"Helvetica-Bold", 12.0f, NULL);
+    CTFontRef myFont = CTFontCreateWithName( (CFStringRef)@"Helvetica-Bold", 10.0f, NULL);
     
     UIColor *fontColor;
-    if ((count < 100) && count > 10) fontColor = [UIColor blackColor];
-    else fontColor = [UIColor whiteColor];
+    fontColor = [UIColor whiteColor];
     
     NSDictionary *attributesDict = [NSDictionary dictionaryWithObjectsAndKeys:
             (__bridge id)myFont, (id)kCTFontAttributeName,
@@ -92,13 +103,11 @@
     CGContextScaleCTM(ctx, 1.0, -1.0);
 
     CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((__bridge CFAttributedStringRef)(stringToDraw));
-    CGSize suggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(
-                                                                        frameSetter, /* Framesetter */
-                                                                        CFRangeMake(0, stringToDraw.length), /* String range (entire string) */
-                                                                        NULL, /* Frame attributes */
-                                                                        CGSizeMake(diameter, diameter), /* Constraints (CGFLOAT_MAX indicates unconstrained) */
-                                                                        NULL /* Gives the range of string that fits into the constraints, doesn't matter in your situation */
-                                                                        );
+    CGSize suggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(frameSetter,
+                                                                        CFRangeMake(0, stringToDraw.length),
+                                                                        NULL,
+                                                                        CGSizeMake(diameter, diameter),
+                                                                        NULL);
     CFRelease(frameSetter);
     
     //Get the position on the y axis
