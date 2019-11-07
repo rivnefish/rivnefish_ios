d563857e9c7ccfed3d887e347235f380675d589d
Anatolii Kyryliuk <anatoliykyryliuk@gmail.com>
2019-11-07 17:11:50 +0100

    Fix icons

--- swift/RivneFish/ui/markerDetails/ContactsCell.swift
+++ swift/RivneFish/ui/markerDetails/ContactsCell.swift
@@ -8,40 +8,28 @@
 
 class ContactsCell: UITableViewCell {
 
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var phoneCaptionLabel: UILabel!
     @IBOutlet weak var phoneCaptionVConstraint: UILabel!
 
-    @IBOutlet weak var phoneVConstraint: NSLayoutConstraint!
-    @IBOutlet weak var phoneButton: UIButton!
-
-    @IBOutlet weak var phoneHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var detailsConstraint: NSLayoutConstraint!
     @IBOutlet weak var detailsLabel: UILabel!
 
     var phoneNumber: String?
 
-    @IBAction func phonePressed(_ sender: UIButton) {
-        if let phone = phoneNumber,
-            let url = URL(string: "tel://+\(phone)") {
-
-            let application:UIApplication = UIApplication.shared
-            if application.canOpenURL(url) {
-                application.openURL(url)
-            }
-        }
-    }
-
     func setup(withPhone phone: String?, details: String?) {
+        textView.isUserInteractionEnabled = true
+        textView.isEditable = false
+        textView.isSelectable = true
+        textView.dataDetectorTypes = [.all]
+        textView.isScrollEnabled = false
+        textView.text = "\(PhoneNumberFormatter.format(phone!)), \(PhoneNumberFormatter.format(phone!))"
+
         let phone = phone ?? ""
         if  !phone.isEmpty {
             phoneNumber = phone
-            phoneButton.setTitle(PhoneNumberFormatter.format(phone), for: UIControlState())
         } else {
             phoneCaptionLabel.text = ""
-            phoneVConstraint.constant = 0
-            phoneButton.setTitle("", for: UIControlState())
-            phoneHeightConstraint.constant = 0
-            phoneVConstraint.constant = 0
         }
         let details = details ?? ""
         if !details.isEmpty {
@@ -50,5 +38,10 @@ class ContactsCell: UITableViewCell {
             detailsLabel.text = ""
             detailsConstraint.constant = 0
         }
+    }
+
+    var privacyAndTermsAttrStr: NSAttributedString {
+        let attrStr = NSMutableAttributedString(string: "Some long attribute string")
+        return attrStr
     }
 }
